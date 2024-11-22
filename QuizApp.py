d1ebc8798d4caed5e675559d5340addd910e8ec6
Muhammad Salman Muqri <144340996+HardinAbAurBhiChota@users.noreply.github.com>
2024-11-22 15:23:04 +0530

    bug fixes

--- QuizApp.py
+++ QuizApp.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import messagebox, simpledialog
+from tkinter import messagebox, simpledialog,Toplevel, Label, Button
 import random
 import pickle
 
@@ -124,3 +124,4 @@ if __name__ == "__main__":
     root = tk.Tk()
     app = QuizApp(root)
     root.mainloop()
+    
